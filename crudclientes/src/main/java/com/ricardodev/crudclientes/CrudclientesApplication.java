2dcf66f4c15a3406e07335259d2765eedad74101
RicardoPereiraDev <odracirDavidPereira@gmail.com>
2025-12-12 20:21:30 +0000

    test

---
+++ crudclientes/src/main/java/com/ricardodev/crudclientes/CrudclientesApplication.java
@@ -0,0 +1,18 @@
+package com.ricardodev.crudclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudclientesApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println(
+				"Hello World"
+		);
+
+		SpringApplication.run(CrudclientesApplication.class, args);
+	}
+
+}
